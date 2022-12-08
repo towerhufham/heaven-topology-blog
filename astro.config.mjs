b3b755e27bc2f91bccb1fed7d14d130b5175a76c
towerhufham <32599109+towerhufham@users.noreply.github.com>
2022-12-07 23:39:26 -0500

    windi

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,15 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-
 import prefetch from "@astrojs/prefetch";
+
+import integrationWindicss from "astro-integration-windicss";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    vue(), 
-    mdx(), 
-    sitemap(), 
-    prefetch({
-      //prefetch all links unless they were given the no-prefetch attribute
-      selector: "a:not([no-prefetch])"
-    })
-  ],
+  integrations: [vue(), mdx(), sitemap(), prefetch({
+    //prefetch all links unless they were given the no-prefetch attribute
+    selector: "a:not([no-prefetch])"
+  }), integrationWindicss()],
   site: "https://heaventopology.com"
 });
